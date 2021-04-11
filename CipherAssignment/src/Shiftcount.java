e9f2e7b675300552ddb25fa6443dce332dcf46ff
Nebaval <Nebaval@DESKTOP-7ENT7QP.lan>
2021-04-11 19:56:11 +0300

    Decryption option added. supports both uppercase and lowercase English Alphalbet

--- CipherAssignment/src/Shiftcount.java
+++ CipherAssignment/src/Shiftcount.java
@@ -1,22 +1,22 @@
 
 public class Shiftcount {
-	
+
 	public static String encrypt(String plainText, int shift) {
-		if(shift>26) {
+		if(shift > 26) {
 			shift = shift%26;
 		}
 		else if(shift<0) {
-			shift = (shift%26)+26;
+			shift = (shift%26) + 26;
 		}
-		
-		String cipherText = "";
-		int length = plainText.length();
-		for (int i = 0; i<length; i++) {
+
+		String cipherText = " ";
+		int length  = plainText.length();
+		for (int i  = 0; i < length; i++) {
 			char b1 = plainText.charAt(i);
 			if(Character.isLetter(b1)) {
 				if(Character.isLowerCase(b1)) {
-					char B = (char)(b1+shift);
-					if(B> 'z') {
+					char B = (char)(b1 + shift);
+					if(B > 'z') {
 						cipherText += (char)(b1 - (26-shift));
 					}
 					else {
@@ -24,9 +24,9 @@ public class Shiftcount {
 					}
 				}
 				else if(Character.isUpperCase(b1)) {
-					char B = (char)(b1+shift);
-					if(B> 'Z') {
-						cipherText += (char)(b1 - (26-shift));
+					char B = (char)(b1 + shift);
+					if(B > 'Z') {
+						cipherText += (char)(b1 - (26 - shift));
 					}
 					else {
 						cipherText += B;
@@ -39,11 +39,55 @@ public class Shiftcount {
 		}
 		return cipherText;
 	}
-public static void main(String[] args) {
-	String text = "Facebook is ComPromized";
-	String cipher = encrypt(text, 4);
-	System.out.println(cipher);
+
+	public static String decrypt(String plainText, int shift) {
+		if(shift > 26) {
+			shift = shift%26;
+		}
+		else if(shift < 0) {
+			shift = (shift%26) + 26;
+		}
+
+		String cipherText = " ";
+		int length  = plainText.length();
+		for (int i  = 0; i < length; i++) {
+			char b1 = plainText.charAt(i);
+			if(Character.isLetter(b1)) {
+				if(Character.isLowerCase(b1)) {
+					char B = (char)(b1 - shift);
+					if(B < 'a') {
+						cipherText += (char)(b1 + (26 - shift));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+				else if(Character.isUpperCase(b1)) {
+					char B = (char)(b1 - shift);
+					if(B < 'A') {
+						cipherText += (char)(b1 + (26 - shift));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+			}
+			else {
+				cipherText += b1;
+			}
+		}
+		return cipherText;
+	}
+
+	public static void main(String[] args) {
+
+		String text   = "My System Is Secured";
+		String cipher = encrypt(text, 4);
+		System.out.println(cipher);
+		String decryption = decrypt(cipher, 4);
+		System.out.println(decryption);
 		
+
 	}
 	
 }
