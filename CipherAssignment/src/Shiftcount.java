d71a215328fb6d9d9ab5c127e9210563eb7f459a
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-04-12 20:58:46 +0300

    Code explanations added and modified to assignment specifications

--- CipherAssignment/src/Shiftcount.java
+++ CipherAssignment/src/Shiftcount.java
@@ -1,93 +1,104 @@
 
 public class Shiftcount {
-
-	public static String encrypt(String plainText, int shift) {
-		if(shift > 26) {
-			shift = shift%26;
-		}
-		else if(shift<0) {
-			shift = (shift%26) + 26;
-		}
-
-		String cipherText = " ";
-		int length  = plainText.length();
-		for (int i  = 0; i < length; i++) {
-			char b1 = plainText.charAt(i);
-			if(Character.isLetter(b1)) {
-				if(Character.isLowerCase(b1)) {
-					char B = (char)(b1 + shift);
-					if(B > 'z') {
-						cipherText += (char)(b1 - (26-shift));
-					}
-					else {
-						cipherText += B;
-					}
-				}
-				else if(Character.isUpperCase(b1)) {
-					char B = (char)(b1 + shift);
-					if(B > 'Z') {
-						cipherText += (char)(b1 - (26 - shift));
-					}
-					else {
-						cipherText += B;
-					}
-				}
-			}
-			else {
-				cipherText += b1;
-			}
-		}
-		return cipherText;
-	}
-
-	public static String decrypt(String plainText, int shift) {
-		if(shift > 26) {
-			shift = shift%26;
-		}
-		else if(shift < 0) {
-			shift = (shift%26) + 26;
-		}
-
-		String cipherText = " ";
-		int length  = plainText.length();
-		for (int i  = 0; i < length; i++) {
-			char b1 = plainText.charAt(i);
-			if(Character.isLetter(b1)) {
-				if(Character.isLowerCase(b1)) {
-					char B = (char)(b1 - shift);
-					if(B < 'a') {
-						cipherText += (char)(b1 + (26 - shift));
-					}
-					else {
-						cipherText += B;
-					}
-				}
-				else if(Character.isUpperCase(b1)) {
-					char B = (char)(b1 - shift);
-					if(B < 'A') {
-						cipherText += (char)(b1 + (26 - shift));
-					}
-					else {
-						cipherText += B;
-					}
-				}
-			}
-			else {
-				cipherText += b1;
-			}
-		}
-		return cipherText;
-	}
-
+	
 	public static void main(String[] args) {
 
-		String text   = "My System Is Secured";
-		String cipher = encrypt(text, 4);
-		System.out.println(cipher);
-		String decryption = decrypt(cipher, 4);
-		System.out.println(decryption);
+		//we use the formula [ShiftCount n]:[Encrypt 0/Decrypt 1]:[Alphabet 0-EN];[Plaintext/Cipher]
 		
+		String text   = "6:0:0:This Is Amazing, I Did It!";
+		String cipher = encrypt(text, 6);
+		System.out.println(cipher);
+		System.out.println("  ");
+		
+		//Decryption of cipher text
+		String cipher1 = "6:1:0:EUA NGBK HKKT IUSVXUSOFKJ, GHUXZ UVKXGZOUT!";
+		String decryption = decrypt(cipher1, 6);
+		System.out.println(decryption);
 
+	}
+	
+	/*creation of Dictionary ("0" for the English Alphabets). 
+	 * - shiftCount represents the number of shifts to be used in defining the key of any encryption */
+	
+	public static String encrypt(String plainText, int shiftCount) {
+		if(shiftCount > 26) {
+			shiftCount = shiftCount%26;
+		}
+		else if(shiftCount < 0) {
+			shiftCount = (shiftCount%26) + 26;
+		}
+
+		//*PlainText definition within the confines of the English Alphabet (0)//
+		
+		String cipherText = " ";
+		int length  = plainText.length();
+		for (int i  = 0; i < length; i++) {
+			char b1 = plainText.charAt(i);
+			if(Character.isLetter(b1)) {
+				if(Character.isLowerCase(b1)) {
+					char B = (char)(b1 + shiftCount);
+					if(B > 'z') {
+						cipherText += (char)(b1 - (26-shiftCount));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+				else if(Character.isUpperCase(b1)) {
+					char B = (char)(b1 + shiftCount);
+					if(B > 'Z') {
+						cipherText += (char)(b1 - (26 - shiftCount));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+			}
+			else {
+				cipherText += b1;
+			}
+		}
+		return cipherText;
+	}
+
+	//Defining our decrpytion formula
+	public static String decrypt(String plainText, int shiftCount) {
+		if(shiftCount > 26) {
+			shiftCount = shiftCount%26;
+		}
+		else if(shiftCount < 0) {
+			shiftCount = (shiftCount%26) + 26;
+		}
+
+		String cipherText = " ";
+		int length  = plainText.length();
+		for (int i  = 0; i < length; i++) {
+			char b1 = plainText.charAt(i);
+			if(Character.isLetter(b1)) {
+				if(Character.isLowerCase(b1)) {
+					char B = (char)(b1 - shiftCount);
+					if(B < 'a') {
+						cipherText += (char)(b1 + (26 - shiftCount));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+				else if(Character.isUpperCase(b1)) {
+					char B = (char)(b1 - shiftCount);
+					if(B < 'A') {
+						cipherText += (char)(b1 + (26 - shiftCount));
+					}
+					else {
+						cipherText += B;
+					}
+				}
+			}
+			else {
+				cipherText += b1;
+			}
+		}
+		return cipherText;
 	}
 	
 }
